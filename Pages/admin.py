55295286617cbb00e366bb802c97f7bdd603ab5d
Zenddd_yeni <adoumazzouz.aa@gmail.com>
2024-06-03 22:04:40 -0400

    Admin Page Decoration done

--- Pages/admin.py
+++ Pages/admin.py
@@ -1,22 +1,91 @@
 from django.contrib import admin
-from .models import Dashboard, Home, Feedback, Podcast, Quest, Step, UserQuestProgress, FeaturedYoutubeVideo, OptIn , OnboardingOption , OnboardingQuestion , SliderImage
+from .models import (
+    Dashboard, Home, Feedback, Podcast, Quest, Step, 
+    UserQuestProgress, FeaturedYoutubeVideo, OptIn, 
+    OnboardingOption, OnboardingQuestion, SliderImage
+)
+from django.utils.html import format_html
 
-# Register your models here.
-admin.site.register(Dashboard)
-admin.site.register(Home)
-admin.site.register(Podcast)
-admin.site.register(Feedback)
-admin.site.register(FeaturedYoutubeVideo)
-admin.site.register(Quest)
-admin.site.register(Step)
-admin.site.register(UserQuestProgress)
-admin.site.register(OptIn)
-admin.site.register(SliderImage)
+
+@admin.register(Dashboard)
+class DashboardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'objectif', 'total_balance', 'total_change_today', 'change_percentage')
+
+    def total_balance(self, obj):
+        return obj.calculate_total_balance()
+
+    def total_change_today(self, obj):
+        return obj.get_changes_today()
+
+    def change_percentage(self, obj):
+        return obj.calculate_change_percentage()
+
+
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'featured_course')
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('feedback_emoji', 'user', 'timestamp')
+    list_filter = ('feedback_choice', 'user', 'timestamp')
+    search_fields = ('user__username', 'user__email')
+
+    def feedback_emoji(self, obj):
+        return obj.get_feedback_choice_display()
+    feedback_emoji.short_description = 'Feedback'
+
+
+@admin.register(Podcast)
+class PodcastAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'banner', 'mp3')
+
+
+@admin.register(FeaturedYoutubeVideo)
+class FeaturedYoutubeVideoAdmin(admin.ModelAdmin):
+    list_display = ('video_id',)
+
+
+@admin.register(Quest)
+class QuestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'points')
+
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    list_display = ('quest', 'title', 'description', 'index', 'points')
+
+
+@admin.register(UserQuestProgress)
+class UserQuestProgressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'quest', 'current_step', 'points_earned')
+    search_fields = ('user__username', 'quest__title')
+
+
+@admin.register(OptIn)
+class OptInAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+
 
 class OnboardingOptionInline(admin.TabularInline):
     model = OnboardingOption
     extra = 1
 
+
 @admin.register(OnboardingQuestion)
 class OnboardingQuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'image')
     inlines = [OnboardingOptionInline]
+
+
+@admin.register(SliderImage)
+class SliderImageAdmin(admin.ModelAdmin):
+    list_display = ('alt_text', 'image_preview')
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px;" />', obj.image.url)
+        return ""
+    image_preview.short_description = 'Preview'
+
