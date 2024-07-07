55c2f0b4a66a0c44424bcc48537442d871f3c789
unknown <darkonex>
2024-07-07 20:01:06 +0100

    vimeo

--- public/static/js/academy.js
+++ public/static/js/academy.js
@@ -147,7 +147,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const videoSRC = document.querySelector(".videoSRC");
                 var lesson_video_container = document.querySelector(".lesson-video");
                 if (response.video.vimeo_url) {
-                    lesson_video_container.innerHTML = '<iframe src="https://player.vimeo.com/video/885359991" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
+                    lesson_video_container.innerHTML = '<div style="padding:56.5% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/980078415?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Shut up your mouth Obama interview during #AMF14 in Dubai"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>';
                 } else if (response.video.video_file) {
                     lesson_video_container.innerHTML = `
                         <video controls controlsList="nodownload">
