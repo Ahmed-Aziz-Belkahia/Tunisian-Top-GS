8ad8b389717bfb558e818c7a2c0a0e32f400444d
Ahmad Aziz Belkahia <ahmadazizbelkahia@gmail.com>
2024-10-11 00:49:59 +0100

    notifications

--- TTG/settings.py
+++ TTG/settings.py
@@ -445,7 +445,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6380)],
+            "hosts": [("redis://:!Ttg12345678@127.0.0.1:6379")],
         },
     },
 }
