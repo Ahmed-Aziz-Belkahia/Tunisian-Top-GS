d98de5bba76d45c8a995ba92026e3881b1db6bb3
Ahmad Aziz Belkahia <ahmadazizbelkahia@gmail.com>
2024-10-11 02:03:44 +0100

    notifications

--- TTG/asgi.py
+++ TTG/asgi.py
@@ -1,14 +1,14 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.sessions import SessionMiddlewareStack
-from Chat.routing import websocket_urlpatterns  # Make sure you have your app's routing set up properly
+from Chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TTG.settings")
+django.setup()  # Ensure Django is fully initialized before importing models
 
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
