540508fcb947d428fbd013ad2dba6be936532da6
Ahmad Aziz Belkahia <ahmadazizbelkahia@gmail.com>
2024-10-11 03:04:07 +0100

    notifications

--- TTG/asgi.py
+++ TTG/asgi.py
@@ -1,4 +1,6 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TTG.settings")
+
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -6,7 +8,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.sessions import SessionMiddlewareStack
 from Chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TTG.settings")
 django.setup()  # Ensure Django is fully initialized before importing models
 
 django_asgi_app = get_asgi_application()
