509c2db7452d3814c7846255b108742453a6a653
Ahmad Aziz Belkahia <ahmadazizbelkahia@gmail.com>
2024-10-11 03:25:18 +0100

    notifications

--- TTG/asgi.py
+++ TTG/asgi.py
@@ -1,16 +1,19 @@
-# TTG/asgi.py
-
 import os
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TTG.settings")
+logger.debug(f"DJANGO_SETTINGS_MODULE: {os.environ['DJANGO_SETTINGS_MODULE']}")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.sessions import SessionMiddlewareStack
-from Chat.routing import websocket_urlpatterns  # Make sure you have your app's routing set up properly
+from Chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TTG.settings")
-
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
