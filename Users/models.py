029d270a326e45f560fa6b0b4fea1cfdaf91fcf4
Ahmad Aziz Belkahia <ahmadazizbelkahia@gmail.com>
2024-10-11 03:02:19 +0100

    notifications

--- Users/models.py
+++ Users/models.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
