eb36a9137d0ca16eb5769ad80f11b1c35d22ae47
Ahmad Aziz Belkahia <ahmadazizbelkahia@gmail.com>
2024-10-30 18:53:38 +0100

    Update

--- Courses/admin.py
+++ Courses/admin.py
@@ -31,7 +31,7 @@ class CourseAdmin(admin.ModelAdmin):
     readonly_fields = ['course_image']
     fieldsets = (
         ('Course Information', {
-            'fields': ('title', 'url_title', 'description', 'price', 'discount_price', 'img', 'course_image', 'professor', 'members_count', 'category', "mini_description", "svg", "mini_landing_img", "landing_img", "fake_enrollment")
+            'fields': ('title', 'url_title', 'description', 'price', 'discount_price', 'img', 'course_image', 'professor', 'members_count', 'category', "mini_description", "svg", "mini_landing_img", "landing_img", "fake_enrollment", "video_count")
         }),
         ('Additional Information', {
             'fields': ('course_requirements', 'course_features', 'video_trailer')
